9a264cd27a54241adeeded1a65c7128994b17346
MisterKrud <162855452+MisterKrud@users.noreply.github.com>
2025-09-11 04:59:06 +0000

    Add iterative function

--- app.js
+++ app.js
@@ -1,2 +1,20 @@
 #!/usr/bin/node
 console.log('test');
+ let fibSequence = []
+const fibs = (num) => {
+   
+    for(let i = 0; i<num ;i++){
+        if(i==0 || i==1){
+            fibSequence.push(i)
+        } else {
+            const newNum = (fibSequence[i-2] + fibSequence[i-1])
+            fibSequence.push(newNum)
+        }
+        
+        
+    }   
+   console.log(fibSequence)
+return fibSequence
+}
+
+
